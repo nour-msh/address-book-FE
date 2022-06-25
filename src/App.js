0e5a2cbc80e57ffbf1c0f1f1948ced98b84b9eba
Nour Mshawrab <mshawrabnour@gmail.com>
2022-06-26 01:05:03 +0300

    Editing home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route ,Link} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import User from "./pages/User";
 import Home from "./pages/Home";
 
--- src/component/About.js
+++ src/component/About.js
@@ -1,1 +1,15 @@
+import React from "react";
+import logo from "../assets/logo.jpg";
 
+function About(){
+    return(
+        <>
+        <div className="aboutContainer">
+        <h1>Welcome to Address Book</h1>
+        <img src={logo} alt="" />
+        </div>
+        </>
+    )
+}
+
+export default About;
